4a447f2d4d3189489ae72f430c505814bdff7c45
Adam Leskis <leskis@gmail.com>
2025-08-04 13:31:00 +0100

    rework the way we populate the etcd unit file

--- scripts/copy_etcd_config_to_controllers.sh
+++ scripts/copy_etcd_config_to_controllers.sh
@@ -1,3 +1,14 @@
+# we also need to get the tailnet IP addresses for each of the controllers, since those need to be passed into each of the etcd
+# config files. etcd can only resolve IP addresses for some of the flags, so we need these available.
+CONTROLLER_1_IP=$(ssh -i ~/.ssh/kubernetes.ed25519 controller-1 "tailscale ip -4" | tr -d '\n\r')
+CONTROLLER_2_IP=$(ssh -i ~/.ssh/kubernetes.ed25519 controller-2 "tailscale ip -4" | tr -d '\n\r')
+CONTROLLER_3_IP=$(ssh -i ~/.ssh/kubernetes.ed24419 controller-3 "tailscale ip -4" | tr -d '\n\r')
+
+# now we swap those into the unit file for the placeholder values
+sed -i "s|CONTROLLER_1_IP|${CONTROLLER_1_IP}|g" ./units/etcd.service
+sed -i "s|CONTROLLER_2_IP|${CONTROLLER_2_IP}|g" ./units/etcd.service
+sed -i "s|CONTROLLER_3_IP|${CONTROLLER_3_IP}|g" ./units/etcd.service
+
 for host in controller-{1..3}; do
   scp -i ~/.ssh/kubernetes.ed25519 \
     downloads/controller/etcd \
