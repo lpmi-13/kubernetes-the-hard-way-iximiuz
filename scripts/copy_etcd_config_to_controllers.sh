23c239f7234566be64b5d402a69ac02aa8b5bb2b
Adam Leskis <leskis@gmail.com>
2025-08-04 13:41:41 +0100

    fix key name

--- scripts/copy_etcd_config_to_controllers.sh
+++ scripts/copy_etcd_config_to_controllers.sh
@@ -2,7 +2,7 @@
 # config files. etcd can only resolve IP addresses for some of the flags, so we need these available.
 CONTROLLER_1_IP=$(ssh -i ~/.ssh/kubernetes.ed25519 controller-1 "tailscale ip -4" | tr -d '\n\r')
 CONTROLLER_2_IP=$(ssh -i ~/.ssh/kubernetes.ed25519 controller-2 "tailscale ip -4" | tr -d '\n\r')
-CONTROLLER_3_IP=$(ssh -i ~/.ssh/kubernetes.ed24419 controller-3 "tailscale ip -4" | tr -d '\n\r')
+CONTROLLER_3_IP=$(ssh -i ~/.ssh/kubernetes.ed25519 controller-3 "tailscale ip -4" | tr -d '\n\r')
 
 # now we swap those into the unit file for the placeholder values
 sed -i "s|CONTROLLER_1_IP|${CONTROLLER_1_IP}|g" ./units/etcd.service
